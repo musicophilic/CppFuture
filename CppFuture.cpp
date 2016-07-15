4b91551fcbef8e5cd3e59e9af7d3330fa11cd371
mayur <mayur@10.252.241.123>
2016-07-15 17:01:23 +0530

    Initial commit

---
+++ CppFuture.cpp
@@ -0,0 +1,45 @@
+
+
+#include <Poco/Thread.h>
+
+#include "Poco/Runnable.h"
+#include "Poco/Thread.h"
+#include <iostream>
+
+using Poco::Runnable;
+using Poco::Thread;
+
+
+class MyThread: public Poco::Runnable
+{
+public:
+    MyThread(const std::string& arg1, int* arg2):
+        _arg1(arg1),
+        _arg2(arg2)
+    {
+    }
+
+    void run()
+    {
+        *_arg2 = 20;
+    }
+
+private:
+    std::string _arg1;
+    int* _arg2;
+};
+
+int* createThread(){
+    int *a = new int;
+    MyThread mt("foo", a);
+    Thread t;
+    t.start(mt);
+    return a;
+}
+
+//...
+int main(){
+    int *my_int = createThread();
+    Thread::sleep(1000);
+    std::cout<<"Before terminating main, my_int is  : "<<*my_int<<std::endl;
+}
